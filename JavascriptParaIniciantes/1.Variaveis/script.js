74353460ea3d3d0cb43ab8004ddba7c20cd55284
Mateus Delai <mateus.takayama@ssys.com.br>
2022-12-16 11:49:31 -0300

    js para iniciantes, variaveis e tipos de daods

---
+++ JavascriptParaIniciantes/1.Variaveis/script.js
@@ -0,0 +1,41 @@
+// Variáveis
+
+var nome = 'Pedro'
+var PossuiFaculdade = true
+var idade = 25
+
+console.log(nome, idade, PossuiFaculdade)
+
+var preco = 25
+var quantidade = 5
+var valorTotal = preco * quantidade
+
+console.log("o total vendido foi de: R$"+valorTotal)
+
+var sobrenome = "Ricardo",
+    cidade = "Pernambuco"
+
+console.log (nome, sobrenome, cidade)
+
+
+/* Exerício:
+
+Declarar:
+
+uma variável com o seu nome
+uma variável com a sua idade
+uma variável com a sua comida favorita e não atribuir valor
+atribuir valor a comida favorita
+5 variáveis diferentes sem valor
+
+*/
+
+var nome = 'mateus'
+var idade = 21
+var comida
+comida = 'strogonoff'
+var profissão,
+    conjugue,
+    cidade,
+    estado,
+    cep
---
+++ JavascriptParaIniciantes/2.TiposDeDados/script.js
@@ -0,0 +1,28 @@
+// Tipos de Dados
+
+var nome = "André"
+var sobrenome = "Pereira"
+var nomeCompleto = nome + " "+sobrenome;
+
+console.log(nomeCompleto)
+
+// Exercício
+
+// Declare uma variável contendo uma string
+var nomePri = "Pedro"
+
+// Declare uma variável contendo um número dentro de uma String
+var gols = "21"
+
+// Declare uma variável com a sua idade
+var idade = "21"
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var primeiroNome = "Joao", segundoNome = "Paulo"
+var fullName = primeiroNome + " " + segundoNome
+
+// Coloque a seguinte frase em uma variável: Its time
+
+var hora = " to go lunch"
+var frase = "Its time" + hora
+console.log(frase)
